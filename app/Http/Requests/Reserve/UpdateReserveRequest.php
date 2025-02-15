90d96ee93de15f1c29f6a2de2c86ab573e7f2002
BardiaJS <bsardroodi@gmail.com>
2025-02-15 01:43:55 -0600

    debug the request files

--- app/Http/Requests/Reserve/UpdateReserveRequest.php
+++ app/Http/Requests/Reserve/UpdateReserveRequest.php
@@ -11,7 +11,7 @@ class UpdateReserveRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class UpdateReserveRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'hour' => ['required', 'date_format:H:i']
         ];
     }
 }
